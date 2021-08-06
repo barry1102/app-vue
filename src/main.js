ab66e20a6942a0881ad1b3d9e610c0b55093b0c0
谭友强 <tanyouqiang@yuanqisenlin.com>
2021-08-06 18:54:43 +0800

    增加cookie.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import cookie from './plugins/cookie'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  cookie,
   render: h => h(App)
 }).$mount('#app')
