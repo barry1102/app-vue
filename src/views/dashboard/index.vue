ea09557ade748ce732fb38854f2436d09797e542
谭友强 <tanyouqiang@yuanqisenlin.com>
2021-08-12 17:21:13 +0800

    dashboard

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dashboard-container">
+    多希望还能再像个孩子，快乐的做个傻子
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+  }
+}
+</script>
